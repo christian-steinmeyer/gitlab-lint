63d10f98946f0f0ddf08806b640820bcc3c17e33
Christian Steinmeyer <christian.steinmeyer@item.fraunhofer.de>
2020-09-22 12:38:09 +0200

    update setup to be usable with pre-commit

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name='gitlab-lint',
-    version='0.2.1',
+    version='0.2.2',
     py_modules=['gitlab-lint'],
-    author="Elijah Roberts",
-    author_email="elijah@elijahjamesroberts.com",
-    description="This is a CLI application to quickly lint .gitlab-ci.yml files using the gitlab api",
+    author="Christian Steinmeyer",
+    author_email="christian.steinmeyer@item.fraunhofer.de",
+    description="This is a CLI application to quickly lint .gitlab-ci.yml files using the gitlab api. It's a fork "
+                "from https://github.com/mick352/gitlab-lint (https://github.com/elijah-roberts/gitlab-lint)",
     license="MIT",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    download_url="https://github.com/elijah-roberts/gitlab_lint/archive/0.2.1.tar.gz",
+    download_url="https://github.com/christian-steinmeyer/gitlab-lint/archive/v0.2.2.tar.gz",
     keywords=['GITLAB', 'LINT', 'GIT'],
     packages=find_packages(),
     classifiers=[
